09a808ef2c42a14e9e84c416ab50ce42ee40c699
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-01-27 16:00:16 -0300

    fixing the topological research and implementing the algorithm to find connected components

--- Dfs.java
+++ Dfs.java
@@ -9,14 +9,20 @@ public class DFS {
 	private List<Integer> discoveryTime;
 	private List<Integer> finalizationTime;
 	private List<String> color;
-	private int time = 1;
+	private int time;
 	private List<Integer> ascendingDiscoveryTime;
 	private List<Integer> topologicalSort;
+	private List<Integer> id;
+	private int count;
 	
 	public DFS(GraphRepresentationByLinkedList graph) {
 		this.graph = new GraphRepresentationByLinkedList(1);
 		this.graph = graph;
 		
+		this.time = 1;
+		this.count = 1;
+		
+		this.id = new ArrayList<>();
 		this.topologicalSort = new ArrayList<>();
 		this.ascendingDiscoveryTime = new ArrayList<>();
 		this.discoveryTime = new ArrayList<Integer>(this.graph.getTotalNodes());
@@ -29,21 +35,40 @@ public class DFS {
 			this.finalizationTime.add(0);
 			this.color.add("white");
 			this.pi.add(0);
+			this.id.add(0);
 		}	
 		
 	}
 	
-	public void dfsGraph() {
+	public void dfsGraph(String mode) {
+		if(mode.equals("normal")) {
+			for(int i = 0; i < this.graph.getTotalNodes(); i++) {
+				if (this.color.get(i) == "white") {
+					dfsVisit(this.graph.arrayOfLinkedLists.get(i), i);
+					this.count++;
+				} 
+			}	
+		}else if(mode.equals("topological")) {
+			List<Integer> sources = this.sourcesList();
+			
+			for(int i = 0; i < sources.size(); i++) {
+				int node = sources.get(i);
 				
-		for(int i = 0; i < this.graph.getTotalNodes(); i++) {
-			if (this.color.get(i) == "white") {
-				dfsVisit(this.graph.arrayOfLinkedLists.get(i), i);
-			} 
-		}
+				for(int j = 0; j < this.graph.getTotalNodes(); j++) {
+					if(this.graph.arrayOfLinkedLists.get(j).linkedList.get(0) == node) {
+						dfsVisit(this.graph.arrayOfLinkedLists.get(j), j);
+						break;
+					}
+				}
+								
+			}	
+		}		
+		
 	}
 	
 	public void dfsVisit(IntegerLinkedList list, int node) {
 		this.color.set(node, "gray");
+		this.id.set(node, count);
 		this.discoveryTime.set(node, this.time);
 		this.time++;
 			
@@ -112,9 +137,117 @@ public class DFS {
 	}
 
 	public List<String> getColor() {
-		return color;
+		return this.color;
 	}
 	
+	public List<Integer> sourcesList(){
+		List<Integer> sources = new ArrayList<>();
+		int node = 0;
+		
+		for(int i = 0; i < this.graph.getTotalNodes(); i++) {
+			boolean isSource = true;
+			node = this.graph.arrayOfLinkedLists.get(i).linkedList.get(0);
+			
+			for(int j = 0; j < this.graph.getTotalNodes(); j++) {
+				if(this.graph.arrayOfLinkedLists.get(j).linkedList.lastIndexOf(node) > 0) {
+					isSource = false;
+					break;
+				}
+			}
+		
+			if(isSource) {
+				sources.add(node);
+			}
+		}
+		
+		return sources;
+	}
 	
+	public List<List<List<Integer>>> getTopologicalEdges() {
+		List<List<List<Integer>>> topologicalEdges = new ArrayList<>();
+		List<List<Integer>> edges = new ArrayList<>();
+		List<Integer> topologicalSort = this.getTopologicalSort();
+		
+		for(int i = 0; i < this.topologicalSort.size(); i++){
+			edges = this.getEdges(topologicalSort.get(i));
+			
+			if(edges.size() > 0) {
+				topologicalEdges.add(edges);
+			}			
+		}
+		
+		return topologicalEdges;
+	}
+	
+	public List<List<Integer>> getEdges(int node){
+		List<List<Integer>> edges = new ArrayList<>();
+		
+		for(int i = 0; i < this.graph.getTotalNodes(); i++) {
+			List<Integer> linkedList = this.graph.arrayOfLinkedLists.get(i).linkedList;
+			
+			if(linkedList.get(0) == node && linkedList.size() > 1) {
+				for(int j = 1; j < linkedList.size(); j++) {
+					List<Integer> edge = new ArrayList<>();
+					edge.add(linkedList.get(0));
+					edge.add(linkedList.get(j));
+					
+					edges.add(edge);
+				}
+				
+				break;
+			}
+		}
+		
+		return edges;
+	}
+	
+	public boolean isConnected(int node1, int node2) {
+		
+		if(this.id.get(this.position(node1)) == this.id.get(this.position(node2))) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public int getId(int node) {
+		return this.id.get(this.position(node));
+	}
+	
+	public int getNumberOfConnectedComponents() {
+		return this.count;
+	}
+	
+	public List<Integer> getConnectedComponentsOfId(int id){
+		List<Integer> connectedComponents = new ArrayList<>();
+		
+		for(int i = 0; i < this.graph.getTotalNodes(); i++) {
+			if(this.id.get(i) == id) {
+				connectedComponents.add(this.graph.arrayOfLinkedLists.get(i).linkedList.get(0));
+			}
+		}
+		
+		return connectedComponents;
+	}
+	
+	public List<List<Integer>>getTheListsOfConnectedComponents() {
+		List<List<Integer>> ListsOfConnectedComponents = new ArrayList<>();
+		
+		for(int i = 1; i < count; i++) {
+			List<Integer> connectedComponents = new ArrayList<>();
+			
+			for(int j = 0; j < this.graph.getTotalNodes(); j++) {
+				int firstElement = this.graph.arrayOfLinkedLists.get(j).linkedList.get(0);
+				
+				if(this.id.get(this.position(firstElement)) == i) {
+					connectedComponents.add(firstElement);
+				}
+			}
+			
+			ListsOfConnectedComponents.add(connectedComponents);
+		}
+		
+		return ListsOfConnectedComponents;
+	}
 }
 
--- DfsTest.java
+++ DfsTest.java
@@ -48,10 +48,9 @@ class DFSTest {
 		graph.addLinkedList(list5);
 		
 		DFS graphDfs = new DFS(graph);
-		graphDfs.dfsGraph();
+		graphDfs.dfsGraph("normal");
 		
 		List<Integer> ascendingDiscoveryTime = graphDfs.getAscendingDiscoveryTime();		
-		List<Integer> topologicalSort = graphDfs.getTopologicalSort();
 		List<String> color = graphDfs.getColor();
 		
 		List<Integer> aDTExpected = new ArrayList<>();
@@ -61,21 +60,345 @@ class DFSTest {
 		aDTExpected.add(3);
 		aDTExpected.add(5);
 		
-		List<Integer> tSExpected = new ArrayList<>();
-		tSExpected.add(1);
-		tSExpected.add(2);
-		tSExpected.add(5);
-		tSExpected.add(4);
-		tSExpected.add(3);
-		
 		List<String> colorExpected = new ArrayList<>();
 		for(int i = 0; i < 5; i++) {
 			colorExpected.add("black");
 		}
 		
 		Assertions.assertEquals(aDTExpected, ascendingDiscoveryTime);
-		Assertions.assertEquals(tSExpected, topologicalSort);
 		Assertions.assertEquals(colorExpected, color);
 	}
+	
+	@Test
+	void topologicalSortingTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(3);
+		
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(3);
+		
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(4);
+		list4.addLinkedListElement(2);
+		list4.addLinkedListElement(5);
+		
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(5);
+		
+		IntegerLinkedList list6 = new IntegerLinkedList(1);
+		list6.addLinkedListElement(6);
+		list6.addLinkedListElement(3);
+		
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+		graph.addLinkedList(list6);
+		
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("topological");
+		
+		List<Integer> topologicalSort = graphDfs.getTopologicalSort();
+		
+		List<Integer> tSExpected = new ArrayList<>();
+		tSExpected.add(6);
+		tSExpected.add(4);
+		tSExpected.add(5);
+		tSExpected.add(1);
+		tSExpected.add(2);
+		tSExpected.add(3);
+		
+		System.out.println("The topological result is: " + topologicalSort);
+		
+		Assertions.assertEquals(tSExpected, topologicalSort);		
+		
+	}
+	
+	@Test 
+	void edgesReturningTest(){
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(3);
+		
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(3);
+		
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		
+		DFS graphDfs = new DFS(graph);
+		
+		List<Integer> edge1 =  new ArrayList<>();
+		edge1.add(2);
+		edge1.add(3);
+		
+		List<List<Integer>> edgesExpected = new ArrayList<>();
+		edgesExpected.add(edge1);
+		
+		List<List<Integer>> edges = graphDfs.getEdges(2);
+		
+		Assertions.assertEquals(edgesExpected, edges);
+	}
+	
+	@Test
+	void topologicalEdgesTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(3);
+		
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(3);
+		
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(4);
+		list4.addLinkedListElement(2);
+		list4.addLinkedListElement(5);
+		
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(5);
+		
+		IntegerLinkedList list6 = new IntegerLinkedList(1);
+		list6.addLinkedListElement(6);
+		list6.addLinkedListElement(3);
+		
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+		graph.addLinkedList(list6);
+		
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("topological");
+		
+		List<List<List<Integer>>> toplogicalEdges = graphDfs.getTopologicalEdges();
+		
+		System.out.println("The edges are: " + toplogicalEdges);
+		
+	}
+	
+	@Test
+	void isConnectedTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(5);
+		
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(1);
+		list2.addLinkedListElement(5);
+		
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		list3.addLinkedListElement(6);		
+		
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(5);
+		list4.addLinkedListElement(1);
+		list4.addLinkedListElement(2);
+		
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(6);
+		list5.addLinkedListElement(3);
+		
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+			
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("normal");
+		
+		boolean connection1 = graphDfs.isConnected(2, 1);
+		boolean connection2 = graphDfs.isConnected(1, 5);
+		boolean connection3 = graphDfs.isConnected(3, 6);
+		boolean connection4 = graphDfs.isConnected(6, 1);
+		boolean connection5 = graphDfs.isConnected(3, 5);
+				
+		Assertions.assertEquals(true, connection1);
+		Assertions.assertEquals(true, connection2);
+		Assertions.assertEquals(true, connection3);
+		Assertions.assertEquals(false, connection4);
+		Assertions.assertEquals(false, connection5);
+		
+	}
+	
+	@Test
+	void getIdTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(5);
 
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(1);
+		list2.addLinkedListElement(5);
+
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		list3.addLinkedListElement(6);		
+
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(5);
+		list4.addLinkedListElement(1);
+		list4.addLinkedListElement(2);
+
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(6);
+		list5.addLinkedListElement(3);
+
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("normal");
+
+		Assertions.assertEquals(1, graphDfs.getId(1));
+		Assertions.assertEquals(1, graphDfs.getId(2));
+		Assertions.assertEquals(1, graphDfs.getId(5));
+		Assertions.assertEquals(2, graphDfs.getId(3));
+		Assertions.assertEquals(2, graphDfs.getId(6));
+		
+	}
+	
+	@Test
+	void getConnectedComponentsOfIdTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(5);
+
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(1);
+		list2.addLinkedListElement(5);
+
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		list3.addLinkedListElement(6);		
+
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(5);
+		list4.addLinkedListElement(1);
+		list4.addLinkedListElement(2);
+
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(6);
+		list5.addLinkedListElement(3);
+
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("normal");
+		
+		List<Integer> connectedComponentsExp1 = new ArrayList<>();
+		connectedComponentsExp1.add(1);
+		connectedComponentsExp1.add(2);
+		connectedComponentsExp1.add(5);
+		
+		List<Integer> connectedComponentsExp2 = new ArrayList<>();
+		connectedComponentsExp2.add(3);
+		connectedComponentsExp2.add(6);
+		
+		List<Integer> connectedComponents1 = graphDfs.getConnectedComponentsOfId(1);
+		List<Integer> connectedComponents2 = graphDfs.getConnectedComponentsOfId(2);
+		
+		Assertions.assertEquals(connectedComponentsExp1, connectedComponents1);
+		Assertions.assertEquals(connectedComponentsExp2, connectedComponents2);
+		
+	}
+	
+	@Test
+	void getTheListsOfConnectedComponentsTest() {
+		//creating lists
+		IntegerLinkedList list1 = new IntegerLinkedList(1);
+		list1.addLinkedListElement(1);
+		list1.addLinkedListElement(2);
+		list1.addLinkedListElement(5);
+
+		IntegerLinkedList list2 = new IntegerLinkedList(1);
+		list2.addLinkedListElement(2);
+		list2.addLinkedListElement(1);
+		list2.addLinkedListElement(5);
+
+		IntegerLinkedList list3 = new IntegerLinkedList(1);
+		list3.addLinkedListElement(3);
+		list3.addLinkedListElement(6);		
+
+		IntegerLinkedList list4 = new IntegerLinkedList(1);
+		list4.addLinkedListElement(5);
+		list4.addLinkedListElement(1);
+		list4.addLinkedListElement(2);
+
+		IntegerLinkedList list5 = new IntegerLinkedList(1);
+		list5.addLinkedListElement(6);
+		list5.addLinkedListElement(3);
+
+		GraphRepresentationByLinkedList graph = new GraphRepresentationByLinkedList(1);
+		graph.addLinkedList(list1);
+		graph.addLinkedList(list2);
+		graph.addLinkedList(list3);
+		graph.addLinkedList(list4);
+		graph.addLinkedList(list5);
+
+		DFS graphDfs = new DFS(graph);
+		graphDfs.dfsGraph("normal");
+		
+		List<Integer> connectedComponentsExp1 = new ArrayList<>();
+		connectedComponentsExp1.add(1);
+		connectedComponentsExp1.add(2);
+		connectedComponentsExp1.add(5);
+		
+		List<Integer> connectedComponentsExp2 = new ArrayList<>();
+		connectedComponentsExp2.add(3);
+		connectedComponentsExp2.add(6);
+		
+		List<List<Integer>> allConnectedComponentsExp = new ArrayList<>();
+		allConnectedComponentsExp.add(connectedComponentsExp1);
+		allConnectedComponentsExp.add(connectedComponentsExp2);
+		
+		List<List<Integer>> allConnectedComponents = graphDfs.getTheListsOfConnectedComponents();
+		
+		Assertions.assertEquals(allConnectedComponentsExp, allConnectedComponents);
+	}
 }
